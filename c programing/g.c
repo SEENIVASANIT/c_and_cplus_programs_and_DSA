9fc80e0ad9e3e4e08a396ba17f67f85041098910
SEENIVASAN_T <89895557+SEENIVASANIT@users.noreply.github.com>
2023-06-30 07:21:03 +0530

    Delete c programing directory

--- c programing/g.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char k[10];
-	printf("ENTER NAME\n");
-	gets(k);
-	printf("%s",k);
-	return 0;
-}
--- c programing/m.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int a=10;
-int main()
-{
-	int o=29;
-	cout<<a;
-}
